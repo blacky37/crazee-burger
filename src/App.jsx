b3eb8c5282aa9fe0a690077d77c710371bd7891d
ihouenou90@gmail.com <ihouenou90@gmail.com>
2025-10-23 21:49:14 +0100

    ✨ F01:LOGIN > Add title and form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
-import { useState } from "react";
 import LoginPage from "./components/pages/LoginPage";
 
 function App() {
-  // state (état, données)
-  const [prenom, setPrenom] = useState("");
-
-  // comportements
-
-  const handleSubmit = () => {
-    alert("Bonjour");
-  };
-
-  // affichage (render)
-
   return <LoginPage />;
 }
 
