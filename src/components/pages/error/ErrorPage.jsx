4fa9a4471038bea2f0fa99ff1e20cb8846460e8f
ihouenou90@gmail.com <ihouenou90@gmail.com>
2025-10-26 00:02:59 +0100

    ✨ F02: APP > add redirection from/to ErrorPage

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router";
+
 export default function ErrorPage() {
   return (
     <div>
       <h1>ErrorPage</h1>
       <br />
-      <button>Retourner à la page d'accueil</button>
+      <Link to="/">
+        <button>Retourner à la page d'accueil</button>
+      </Link>
     </div>
   );
 }
